7455093fc3eb3b121d01a2d459f763ed0ee951a5
Khambay Phasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-30 18:49:37 +0000

    committing data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+let users = [];
+
+users.push({name:"Jason", email:"Jason@email.com"}); 
+users.push({name:"Jack", email:"Jack@email.com"}); 
+users.push({name:"Sarah", email:"Sarah@email.com"}); 
+users.push({name:"Lilly", email:"Lilly@email.com"}); 
+
+
+module.exports = users;

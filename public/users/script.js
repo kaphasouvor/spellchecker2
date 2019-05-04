8076823da9a846071d55e89e4a5348da89fc834a
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-05-03 20:13:52 -0400

    Add files via upload

---
+++ public/users/script.js
@@ -0,0 +1,15 @@
+/* global fetch */
+// this evironment will be in the user's broswer ex:public
+
+function handleClick(){
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  
+  console.log(name);
+  fetch("/getemail" + "?" +"name=" + name + "&" + "phone=" + phone)
+    .then(response=>response.text())
+    .then(function(response){
+      console.log(response);
+      document.getElementById("results").innerHTML = response;
+    });
+}
